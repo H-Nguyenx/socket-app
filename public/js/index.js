61326066346c5903b937a7c02f62de983f4ecaea
H-Nguyenx <hungnguyen92@outlook.com>
2017-11-12 18:15:03 +0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
    console.log('Connected to server');
-   socket.emit('createMessage', {
-      from: 'me',
-      text: 'hi, that works',
-   });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
    console.log('new user connected');
 
-   socket.emit('newMessage', {
-      from: 'abc@abc.com',
-      text: 'Hello',
-      createdAt: 123123
-   });
-
    socket.on('createMessage', (message) => {
       console.log('Message received:', message);
+      io.emit('newMessage', {
+         from: message.from,
+         text: message.text,
+         createdAt: new Date().getTime()
+      });
    });
 
    socket.on('disconnect', () => {
