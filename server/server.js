a6ce57330d4902182ed48122e9111264d8792ff1
H-Nguyenx <hungnguyen92@outlook.com>
2017-11-13 17:39:27 +0700

    Add message form and message display function

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 
 const express = require('express');
 const port = process.env.PORT || 3000;
@@ -13,14 +14,13 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
    console.log('new user connected');
+   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-   socket.on('createMessage', (message) => {
-      console.log('Message received:', message);
-      io.emit('newMessage', {
-         from: message.from,
-         text: message.text,
-         createdAt: new Date().getTime()
-      });
+   socket.on('createMessage', (message, callback) => {
+      console.log('createMessage', message);
+      io.emit('newMessage', generateMessage(message.from, message.text));
+      callback('This is from the server');
    });
 
    socket.on('disconnect', () => {
